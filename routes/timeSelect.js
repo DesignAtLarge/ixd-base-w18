69a4ca025eae995d73253bc7b26da278e047991f
Carlos Chulo <cchulo@ucsd.edu>
2018-02-17 17:27:59 -0800

    attempting to fix timezone issue

--- routes/timeSelect.js
+++ routes/timeSelect.js
@@ -5,7 +5,7 @@ var now = new Date();
 var oneHourLater = new Date();
 oneHourLater.setHours(now.getHours() + 1);
 
-var moment = require('moment');
+var moment = require('moment-timezone');
 
 Date.prototype.addHours = function(h){
 	//var copiedDate = new Date();
@@ -15,8 +15,11 @@ Date.prototype.addHours = function(h){
 }
 
 exports.view = function(req, res){
+	let curr = moment().tz('America/Los_Angeles');
 	res.render('timeSelect', {
-		'currentTime':( ("0"+now.getHours()).slice(-2) + ":" +("0"+now.getMinutes()).slice(-2)),
-		'nextTime':( ("0"+oneHourLater.getHours()).slice(-2) + ":" +("0"+oneHourLater.getMinutes()).slice(-2))
+		// 'currentTime':( ("0"+now.getHours()).slice(-2) + ":" +("0"+now.getMinutes()).slice(-2)),
+		// 'nextTime':( ("0"+oneHourLater.getHours()).slice(-2) + ":" +("0"+oneHourLater.getMinutes()).slice(-2))
+		currentTime : curr.hours() + ":" + curr.minutes(),
+		nextTime : curr.add(moment.duration(1, "hours")).hours() + ":" + curr.minutes()
 	});
 };
