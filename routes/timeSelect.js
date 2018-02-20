a81644e7775da1bd9ca72238f3c641c5aa749440
Carlos Chulo <cchulo@ucsd.edu>
2018-02-20 08:31:40 -0800

    added the prefixed 0 that was there before, avoids errors when time is < 12

--- routes/timeSelect.js
+++ routes/timeSelect.js
@@ -1,25 +1,11 @@
-/*
- * GET home page.
- */
-var now = new Date();	
-var oneHourLater = new Date();
-oneHourLater.setHours(now.getHours() + 1);
-
 var moment = require('moment-timezone');
-
-Date.prototype.addHours = function(h){
-	//var copiedDate = new Date();
-	var copiedDate = moment().tz('America/Los_Angeles').add(moment.duration(h, "hours"));
-    //copiedDate.setTime(this.getTime() + (h*60*60*1000)); 
-    return copiedDate.toDate();
-}
 
 exports.view = function(req, res){
 	let curr = moment().tz('America/Los_Angeles');
 	res.render('timeSelect', {
 		// 'currentTime':( ("0"+now.getHours()).slice(-2) + ":" +("0"+now.getMinutes()).slice(-2)),
 		// 'nextTime':( ("0"+oneHourLater.getHours()).slice(-2) + ":" +("0"+oneHourLater.getMinutes()).slice(-2))
-		currentTime : curr.hours() + ":" + curr.minutes(),
-		nextTime : curr.add(moment.duration(1, "hours")).hours() + ":" + curr.minutes()
+		currentTime : ("0" + curr.hours()).slice(-2) + ":" + curr.minutes(),
+		nextTime : ("0" + curr.add(moment.duration(1, "hours")).hours()).slice(-2) + ":" + curr.minutes()
 	});
 };
