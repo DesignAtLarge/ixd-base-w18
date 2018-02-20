48ce0ac0478cfe0b24b8feeb0688b1caddf1a05c
Carlos Chulo <cchulo@ucsd.edu>
2018-02-20 10:30:10 -0800

    can now GET and POST users

--- routes/apiUser.js
+++ routes/apiUser.js
@@ -1,9 +1,43 @@
+var User = require('../models/user');
+
 exports.get = (req, res) => {
-    res.status(200).json("GET user, TODO");
+
+    var id = req.params.id;
+    
+    User.findById(id, (err, user) => {
+        if(err)
+            res.status(500).json({
+                message: "could not get user",
+                user   : undefined
+            });
+        res.status(200).json({
+            message: "user found",
+            user : user
+        });
+    });
 }
 
 exports.post = (req, res) => {
-    res.status(200).json("POST user, TODO");
+    console.log(req.body);
+
+    var user = new User();
+    user.idToken = req.body.idToken;
+    user.name = req.body.name;
+    user.history = req.body.history;
+
+    user.save( err => {
+        if(err)
+            res.status(500).json({
+                message : "could not create user",
+                user    : undefined
+            });
+        
+        res.status(200).json({
+            message : "user created",
+            user : user
+        });
+    });
+    
 }
 
 exports.put = (req, res) => {
