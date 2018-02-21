6403c7a51d2c18e1121af437798795a88f452093
Carlos Chulo <cchulo@ucsd.edu>
2018-02-20 18:36:01 -0800

    can now create users with facebook authentication

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,6 +2,22 @@ function authFB(res){
     console.log("authenticating with Facebook");
 
     console.log(res);
+
+    $.get("/api/user/" + res.id, (response) => {
+        console.log(response);
+        if(response.user.length === 0){
+            console.log("no user found, attempting to create user...");
+            $.post("/api/user", { name: res.name, idToken: res.id, history: []}, (res) => {
+                console.log(res);
+            })
+        } else {
+            console.log("user found: " + response.user[0])
+        }
+    });
+
+    // $.get("/api/user/0", (res) => {
+    //     console.log(res);
+    // });
 }
 
 function authGPlus(res){
