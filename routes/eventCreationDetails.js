6ef4bebab72800a48a551f07cbe0bbd101cb7bc8
Aishma Raghu <airaghu@ucsd.edu>
2018-02-25 16:52:25 -0800

    UI changes for Event Creation Details

--- routes/eventCreationDetails.js
+++ routes/eventCreationDetails.js
@@ -4,6 +4,7 @@
  */
 
 var moment = require('moment-timezone');
+var tagnames = require('../public/json/tags.json');
 
 exports.view = function(req, res){
 	let curr = moment().tz('America/Los_Angeles');
@@ -11,7 +12,8 @@ exports.view = function(req, res){
 		// 'currentTime':( ("0"+now.getHours()).slice(-2) + ":" +("0"+now.getMinutes()).slice(-2)),
 		// 'nextTime':( ("0"+oneHourLater.getHours()).slice(-2) + ":" +("0"+oneHourLater.getMinutes()).slice(-2))
 		currentTime : ("0" + curr.hours()).slice(-2) + ":" + curr.minutes(),
-		nextTime : ("0" + curr.add(moment.duration(1, "hours")).hours()).slice(-2) + ":" + curr.minutes()
+		nextTime : ("0" + curr.add(moment.duration(1, "hours")).hours()).slice(-2) + ":" + curr.minutes(),
+		tags : tagnames.tags
 	});
 };
 
