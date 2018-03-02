4fdf7396d0e92072d580d6f67598ec9469758c0b
Carlos Chulo <cchulo@ucsd.edu>
2018-03-01 17:21:35 -0800

    added more meta data to event.js and added new model, tagReview.js, for real reviews.

--- models/event.js
+++ models/event.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('event', {
     name : {type : string, default: '', required: true},
     description : {type : string, default: '', required: true},
+    imageURL : {type: string, default: '', required: true},
     lat  : {type : number, default: 0, required: true},
     long : {type : number, default: 0, required: true}
 });
