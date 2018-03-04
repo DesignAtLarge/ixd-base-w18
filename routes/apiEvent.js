3be0a71155a09734c922f3616405c02092cea8df
Carlos Chulo <cchulo@ucsd.edu>
2018-03-04 12:08:21 -0800

    implemented event post route, fixed time display in event creation page

--- routes/apiEvent.js
+++ routes/apiEvent.js
@@ -4,7 +4,19 @@ exports.get = (req, res) => {
     var id = req.params.id;
 
     if(id !== undefined)
-        Event.findById(id, (err, event) => {});
+        Event.findById(id, (err, event) => {
+            if(err){
+                res.status(500).json({
+                    message: "error retrieving events",
+                    events: undefined
+                });
+                return;
+            }
+            res.status(200).json({
+                message: "success",
+                event : event
+            })
+        });
     else
         Event.find({}, (err, events) => {
             if(err){
@@ -23,7 +35,29 @@ exports.get = (req, res) => {
 
 exports.post = (req, res) => {
     var newEvent = new Event();
-    res.status(200).json("POST event, TODO");
+    
+    newEvent.name = req.body.name;
+    newEvent.description = req.body.description;
+    newEvent.imageURL = req.body.imageURL;
+    newEvent.lat = req.body.lat;
+    newEvent.long = req.body.long;
+
+    newEvent.save(err => {
+        if(err){
+            console.error(err);
+            res.status(500).json({
+                message: "could not create event",
+                event: undefined
+            });
+            return;
+        }
+        console.log(newEvent)
+        res.status(200).json({
+            message: "success",
+            event: newEvent
+        })
+    });
+    
 }
 
 exports.put = (req, res) => {
