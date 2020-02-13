a5ff0d9f10b688670c1f502cd743567c09c4efa2
eminguyen <emilyhuongnguyen@gmail.com>
2020-02-13 07:00:55 -0800

    Add Login Page component

--- front-end/src/components/LoginPage/index.jsx
+++ front-end/src/components/LoginPage/index.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
-import { Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 
+import boba from '../../assets/boba.svg';
 import './style.less';
 
 const LoginPage = props => {
   return (
-    <form onSubmit={props.handleSubmit} className="login-form">
-      <Form.Item>
-        <Input
-          name="username"
-          className="form-input"
-          value={props.values.username}
-          onChange={props.handleChange}
-          onBlur={props.handleBlur}
-          size="large"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Input
-          name="password"
-          className="form-input"
-          value={props.values.password}
-          onChange={props.handleChange}
-          onBlur={props.handleBlur}
-          size="large"
-        />
-      </Form.Item>
-    </form>
+    <div className="login-page">
+      <div className="content">
+        <img src={boba} alt="boba"/>
+        <h1> bubbl•t</h1>
+        <form onSubmit={props.handleSubmit} className="login-form">
+          <Form.Item>
+            <Input
+              name="username"
+              className="form-input"
+              placeholder="Username"
+              value={props.values.username}
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              name="password"
+              className="form-input"
+              placeholder="Password"
+              value={props.values.password}
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button className="button">Login</Button>
+          </Form.Item>
+        </form>
+      </div>
+    </div>
   );
 };
 
