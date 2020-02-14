6f2d1449ad7fb36c34ff6ae8d1f171b6d7bbf21e
eminguyen <emilyhuongnguyen@gmail.com>
2020-02-14 11:11:53 -0800

    Remove useless button tag

--- front-end/src/components/BobaCarousel/index.jsx
+++ front-end/src/components/BobaCarousel/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Carousel } from 'antd';
+import { Button, Carousel } from 'antd';
 import Drink from '../Drink';
 
 import './style.less';
@@ -20,14 +20,14 @@ const BobaCarousel = (props) => {
       <Carousel dotPosition="bottom" afterChange={props.onChange} {...settings}>
         {props.drinks.map((drink) => {
           return (
-            <button className="carousel-item" key={'caro' + drink[0]} onClick={props.onClick}>
+            <div className="carousel-item" key={'caro' + drink[0]} onClick={props.onClick}>
               <h1>{drink[0]}</h1>
               <div className="carousel-drink">
                 <Drink
                   color={drink[1].color}
                 />
               </div>
-            </button>
+            </div>
           );
         })}
       </Carousel>
