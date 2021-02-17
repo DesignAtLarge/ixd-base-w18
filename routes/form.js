046c5959ab3ecb6598327c5f0ee6d9d9cb45978e
dingxuan2000 <dingxuan0112@gmail.com>
2021-02-17 22:23:34 +0800

    form page 2.0

--- routes/form.js
+++ routes/form.js
@@ -4,5 +4,11 @@
  */
 
 exports.view = function(req, res){
-    res.render('form');
+    res.render('form'); //show the form page
+    //once clicked the back arrow, go to begin page
+    // const button = document.getElementById('back');
+    // button.addEventListener('click', function(e){
+    //     console.log("Back arrow is clicked!");
+    
+    // })
   };
