be03fd534daccfe55ad8f02d906e8026822ebb6b
Lucas Ryan <lkryan@ucsd.edu>
2021-02-26 12:10:40 -0800

    updates

--- public/js/event.js
+++ public/js/event.js
@@ -30,3 +30,13 @@ function clickHeart2() {
         image.src = "images/heart_outline.png";
     }
 }
+function clickHeart3() {
+    var image = document.getElementById('clickHeart3');
+
+    if (image.src.match("images/heart_outline.png")) {
+        image.src = "images/pink_heart.png";
+    }
+    else {
+        image.src = "images/heart_outline.png";
+    }
+}
