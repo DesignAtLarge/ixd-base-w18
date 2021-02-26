fc4f3b6048bf741322691353031d9eddeca5a268
Lucas Ryan <lkryan@ucsd.edu>
2021-02-25 23:08:51 -0800

    Added a shiton of hearts

--- public/js/event.js
+++ public/js/event.js
@@ -8,3 +8,25 @@ function clickHeart() {
         image.src = "images/heart_outline.png";
     }
 }
+
+function clickHeart1() {
+    var image = document.getElementById('clickHeart1');
+
+    if (image.src.match("images/heart_outline.png")) {
+        image.src = "images/pink_heart.png";
+    }
+    else {
+        image.src = "images/heart_outline.png";
+    }
+}
+
+function clickHeart2() {
+    var image = document.getElementById('clickHeart2');
+
+    if (image.src.match("images/heart_outline.png")) {
+        image.src = "images/pink_heart.png";
+    }
+    else {
+        image.src = "images/heart_outline.png";
+    }
+}
